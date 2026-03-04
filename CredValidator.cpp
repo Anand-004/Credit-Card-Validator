1e5d0c6b6b8c1c8adb56e0dab2395a892b618c90
Anand-004 <anandsince2004@gmail.com>
2026-03-04 22:33:02 +0530

    1st Commit

---
+++ CredValidator.cpp
@@ -0,0 +1,61 @@
+// Credit card Validator program Using Luhn Algorithm
+
+#include <iostream>
+
+bool checkValid(std::string number);
+
+int main()
+{
+    // Getiing Card number from user as input
+    std::string number;
+    std::cout << "#--------Credit card Validator----------#" << "\n";
+    std::cout << "\n"
+              << "Enter Your Credit Card Number : ";
+    std::cin >> number;
+
+    // Validation Function Call
+    bool isValid = checkValid(number);
+
+    // Showing result
+    if (isValid)
+    {
+        std::cout << "\n The number " << number << " is a valid Credit Card Number";
+    }
+    else
+    {
+        std::cout << "\n The number " << number << " is NOT a VAlid Credit Card Number";
+    }
+}
+
+bool checkValid(std::string number)
+{
+    bool isEven = false;
+    int sum = 0;
+    for (int i = 0; i < number.length(); i++)
+    {
+        if (!isEven)
+        {
+            sum += number[i] - '0';
+        }
+        else
+        {
+            int num = number[i] * 2;
+            if (num > 9)
+            {
+                num = num % 10 + num / 10;
+            }
+            sum += num;
+        }
+        isEven = !isEven;
+    }
+
+        if (sum % 10 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+}
